3e0f712ab89860165a1fbf26f5049959bcf90d6f
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2023-09-16 22:00:41 -0300

    Add files via upload

---
+++ src/make.sh
@@ -0,0 +1,3 @@
+g++ Bus.cpp Cartridge.cpp Mapper_002.cpp Mapper_001.cpp Mapper_000.cpp Mapper_003.cpp Mapper_004.cpp Mapper_066.cpp Mapper.cpp olc2A03.cpp olc2C02.cpp olc6502.cpp olcNes_Sounds1.cpp -o main -lX11 -lGL -lpng -lasound
+
+./main
